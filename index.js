54e17601bc01f549ae809c41ede9e71c391e44d0
novotmike <novotasscz@gmail.com>
2016-11-25 17:55:15 +0100

    Finished lora handler

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-var serialport = require("serialport");
+var serialport  = require("serialport");
 var SerialPort = serialport.SerialPort;
+var osutils = require('os-utils');
+var _ = require('lodash');
+var ps = require('current-processes');
 var serialPort;
 var delay=100;
 
 
-serialPort = new SerialPort("COM5", {
+serialPort = new SerialPort("COM4", {
     baudrate: 57600,
     parser: serialport.parsers.readline("\r\n")
 });
@@ -59,13 +62,74 @@ function init(){
 		serialPort.write("mac join abp\r\n");
 	}, 2000);
 
-	setTimeout(function(){ 
+  //MESSAGE FORMAT: MSGNUMBER;
+	/*setTimeout(function(){ 
 		console.log("mac tx cnf 1 CAFECAFECAFE")
 		serialPort.write("mac tx cnf 1 CAFECAFECAFE\r\n")
-	}, 3000);
+	}, 3000);*/
+
+  setTimeout(function(){ 
+    getData();
+  }, 3000);
   	
-
-  	//https://api.pripoj.me/message/get/0004A30B001A66D6?token=84YTv1eXGqfwcZikLYkqWzR4aoMAyqDZ
-
+  var interval = setInterval(function() {
+    getData();
+  }, 60*1000);
   
+}
+
+function getData() {
+  var uptime = (osutils.sysUptime()/100/60).toFixed(0);
+  var proc = {};
+  ps.get(function(err, processes) {
+    processes.forEach(function(process) {
+      if(proc[process.name] === undefined) {
+        proc[process.name] = process.cpu;
+      }
+    });
+    var processText = "";
+    Object.keys(proc).forEach(function(procT) {
+      processText+=procT+";";
+    });
+    //Add uptime
+    var sendData = uptime+";"+processText;
+    sendDataLora(sendData);
+    console.log("SENDING DATA...");
+  });
+}
+
+function sendDataLora(data) {
+  var messagesCount = Math.ceil(data.length/100);
+  console.log("Messages Count: "+messagesCount);
+  for(var i = 0; i < messagesCount; i++) {
+    var add = "";
+    var addM = "";
+    if(i/10 < 1) {add = "0";}
+    if(messagesCount/10 < 1) {addM = "0";}
+    var messageId = addM+messagesCount+add+i;
+    var messageMessage = data.substring(i*100, (i*100)+100);
+    //console.log(toHex(messageId+messageMessage).length);
+    sendDataUART(toHex(messageId+messageMessage), messageId, i);
+  }
+}
+
+function sendDataUART(data, id, i) {
+  setTimeout(function() {
+      console.log("mac tx uncnf 1 "+id);
+      serialPort.write("mac tx uncnf 1 "+data+"\r\n");
+    }, 3000*i);
+} 
+
+function toHex(str,hex){
+  try{
+    hex = unescape(encodeURIComponent(str))
+    .split('').map(function(v){
+      return v.charCodeAt(0).toString(16)
+    }).join('')
+  }
+  catch(e){
+    hex = str
+    console.log('invalid text input: ' + str)
+  }
+  return hex
 }
